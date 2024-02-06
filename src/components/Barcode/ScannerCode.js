4f29cdca65cf8a41bc86c99292aebe5180f08ef2
diarhajrizi <87374424+DiarHajrizi@users.noreply.github.com>
2024-02-06 16:28:52 +0100

    Updated ScannerCode Function

--- src/components/Barcode/ScannerCode.js
+++ src/components/Barcode/ScannerCode.js
@@ -1,35 +1,16 @@
-import React, { useState, useEffect, useCallback } from "react";
+import useScanDetection from "use-scan-detection-react18";
 
-const BarcodeScanner = ({ onBarcodeScanned }) => {
-  const [scannedCode, setScannedCode] = useState("");
-
-  const handleBarcodeScan = useCallback(
-    (event) => {
-      if (event.key === "Enter" && scannedCode.length > 0) {
-        console.log("Scanned barcode:", scannedCode);
-
-        if (typeof onBarcodeScanned === "function") {
-          onBarcodeScanned(scannedCode);
-        }
-
-        setScannedCode("");
-      } else if (event.key !== "Enter" && event.key.length === 1) {
-        setScannedCode((prevCode) => prevCode + event.key);
+const ScannerCode = ({ onBarcodeScanned }) => {
+  useScanDetection({
+    onComplete: (code) => {
+      console.log({ code });
+      if (code) {
+        onBarcodeScanned(code);
+      } else {
+        return null;
       }
     },
-    [scannedCode, onBarcodeScanned]
-  );
-
-  useEffect(() => {
-    const eventListener = (event) => handleBarcodeScan(event);
-    document.addEventListener("keydown", eventListener);
-
-    return () => {
-      document.removeEventListener("keydown", eventListener);
-    };
-  }, [handleBarcodeScan]);
-
-  return null;
+  });
 };
 
-export default BarcodeScanner;
+export default ScannerCode;
